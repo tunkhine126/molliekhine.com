2aaa8342bd44b73530f2733fd2476b122e71aa90
Tun Khine <tunkhine126@gmail.com>
2024-08-06 10:06:31 -0400

    Copy update and firebase build & deploy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/footer'
 function App() {
   return (
     <div className="App w-screen min-h-screen mx-auto pt-4 sm:pt-36">
+      <header class="pb-4 sm:pb-12 text-4xl text-secondary">Hi, I'm Mollie.</header>
       <div className="App-container bg-white items-center justify-center sm:flex sm:max-w-1/2 sm:h-3/6">
         <Image />
         <About />
--- src/components/about.js
+++ src/components/about.js
@@ -7,19 +7,28 @@ function about() {
         <span className="font-medium">VP of Learning & Community</span> at <a href="https://flatironschool.com/" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>Flatiron School</a>. (Check out the <a href="https://www.bletchley.org/" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>industry community</a> we’re building for technologists and creatives.)
       </p>
       <p className="p-2 sm:p-4 text-start">
-        <span className="font-medium">Strategic meeting designer</span> at <a href="http://Meetinglab.co" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>Meeting Lab</a>. 
+        <span className="font-medium">Founder & Strategic Meeting Designer</span> at <a href="http://Meetinglab.co" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>Meeting Lab</a>. 
       </p>
       <p className="p-2 sm:p-4 text-start">
-        <span className="font-medium">Workshop Facilitator & Speaker</span>. <a href="https://www.withconfetti.com/product/virtual-communication-skills-workshop" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>How to Ask Better Questions</a> is a favorite session created  to stick.
+        <span className="font-medium">Workshop Facilitator & Speaker</span>. <a href="https://www.withconfetti.com/product/virtual-communication-skills-workshop" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>How to Ask Better Questions</a> is a fan favorite for teams of all sizes.
       </p>
       <p className="p-2 sm:p-4 text-start">
-        <span className="font-medium">Executive Coach</span>.
+        <span className="font-medium">Executive Coach</span>. Everyone needs a <a href="https://flatironschool.com/blog/a-career-coach-in-your-corner/" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>coach in your corner</a>.
       </p>
       <p className="p-2 sm:p-4 text-start">
-        <a href="https://medium.com/@meetinglab/what-is-a-jeffersonian-dinner-9f26b163c6fd" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>Dinner party host</a> and <a href="https://medium.com/@meetinglab" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>aspiring author</a>.
+        <a href="https://medium.com/@meetinglab/what-is-a-jeffersonian-dinner-9f26b163c6fd" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>Dinner party host</a> and <a href="https://medium.com/@meetinglab" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>occasional writer</a>.
       </p>
       <p className="p-2 sm:p-4  text-start">
-        <span className="font-medium">Outdoor enthusiast</span>, tracking my <a href="https://www.1000hoursoutside.com/trackers" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>1000 hours</a> and <a href="https://www.waituntil8th.org/" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>Wait until 8th advocate</a>.
+        <span className="font-medium">Outdoor enthusiast</span>, tracking my <a href="https://www.1000hoursoutside.com/trackers" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>1000 hours</a>.
+      </p>
+      <p className="p-2 sm:p-4  text-start">
+        <span className="font-medium">Lifelong learner</span>. Currently on a roll with a 500 day streak on DuoLingo.
+      </p>
+      <p className="p-2 sm:p-4  text-start">
+        <span className="font-medium">Mama</span> to 2 beautiful kidsos and <a href="https://www.waituntil8th.org/" target="_blank" rel="noreferrer" className='text-color-normal hover:underline'>Wait until 8th advocate</a>.
+      </p>
+      <p className="p-2 sm:p-4  text-start">
+        <span className="font-medium">Cancer patient</span>, living in the moment and cheering on others who are doing the same..
       </p>
     </div>
   );
